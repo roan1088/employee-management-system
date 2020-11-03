c32d18827bd5104549f7b7da75bc01a2b5c63c55
roan1088 <rohan_188@yahoo.com>
2020-11-03 18:25:18 -0500

    Added prompts and viewing all departments

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,10 @@
 // Importing the db connection
 const connection = require("./connection");
+// Importing the util module
+const util = require("util");
+
+// Promisify the connection query method
+const query = util.promisify(connection.query).bind(connection);
 
 // Object Relational Mapper (ORM)
 
@@ -8,7 +13,17 @@ const connection = require("./connection");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 const orm = {
-    
+    // Method to view all departments
+    viewAllDepartments: async function() {
+        // Query
+        const result = await query("SELECT * FROM department");
+        console.table(result);
+    },
+    // Method to exit
+    exit: function() {
+        // End connection
+        connection.end();
+    }
 };
 
 // Exporting the orm
--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const inquirer = require("inquirer");
 const orm = require("./config/orm");
 
 // Main function
-function main() {
-    
+async function main() {
+    // Variable to store users action
+    let action = "";
+    // Keep repeating until user chooses 
+    while (action !== "Exit") {
+        // Ask user their choice
+        const response = await inquirer.prompt({
+            type: "list",
+            message: "What would you like to do?",
+            name: "action",
+            choices: ["View All Departments", "Exit"]
+        });
+        // Set action to users response
+        action = response.action
+
+        // Perfrom action
+        switch(action) {
+            case "View All Departments":
+                await orm.viewAllDepartments();
+                break;
+            case "Exit":
+                orm.exit();
+                break;
+        }
+    }
 }
 
 // Code flow begin by calling main
